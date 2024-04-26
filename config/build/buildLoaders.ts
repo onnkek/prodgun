b9abcf3904a2e3a2fb5941060cf134e6e95838bf
onnkek <onnkek@gmail.com>
2024-04-26 16:36:38 +0500

    decompose config, add devServer and env variable

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,14 @@
+import webpack from 'webpack';
+
+export default function buildLoaders(): webpack.RuleSetRule[] {
+  
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: 'ts-loader',
+    exclude: /node_modules/,
+  };
+
+  return [
+    typescriptLoader,
+  ]
+}
