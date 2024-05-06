97ec009f3eacfbb3c15fa9b77f672cf819881f9a
onnkek <onnkek@gmail.com>
2024-05-07 02:36:16 +0500

    fix App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,10 @@ import { useTheme } from 'app/providers/ThemeProvider';
 import { Suspense, useEffect } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Navbar/Sidebar';
+import { Sidebar } from 'widgets/Sidebar';
 import { AppRouter } from './providers/router';
 import './styles/index.sass';
+
 
 const App = () => {
   const { theme } = useTheme();
