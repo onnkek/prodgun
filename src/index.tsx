f80d708115cd87f0e5281eb29a46cff9951da936
onnkek <onnkek@gmail.com>
2024-05-07 03:37:22 +0500

    fix index.ts root and add pipeline screenshot testing

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import 'shared/config/i18n/i18n';
 import App from './app/App';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
-const container = document.getElementById('app');
+const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
   <BrowserRouter>
